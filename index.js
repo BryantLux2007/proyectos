ae087f584ed3467d2c1b5e928f3e7286f5796351
BryantLux2007 <bryantgaeldeniz@gmail.com>
2024-09-13 20:25:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-// Obtener los elementos necesarios
 const formulario = document.getElementById('formulario');
 const mensajeEnviado = document.getElementById('mensaje-enviado');
 
-// Escuchar el evento submit del formulario
 formulario.addEventListener('submit', function(event) {
     event.preventDefault(); // Evitar que el formulario se envíe
 
-    // Mostrar el mensaje de confirmación
     mensajeEnviado.style.display = 'block';
 
-    // Ocultar el mensaje después de 3 segundos
     setTimeout(() => {
         mensajeEnviado.style.display = 'none';
     }, 3000);
 
-    // Borrar los datos del formulario
     formulario.reset();
 });
